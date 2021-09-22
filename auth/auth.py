0f4a29d2c14ddce04e9e8ec0e4d68afa5ecffe06
Pavel Nikishin <75571015+l4neboy@users.noreply.github.com>
2021-09-22 19:30:35 +0000

    my mind

--- auth/auth.py
+++ auth/auth.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template,  redirect, url_for, request, flash
 from werkzeug.security import generate_password_hash, check_password_hash
 from models import User
 from models import db
-from flask_login import login_user, current_user
+from flask_login import login_user, current_user, login_required
 
 auth = Blueprint('auth', __name__, template_folder='templates')
 
@@ -57,5 +57,6 @@ def logout():
     return 'Logout'
 
 @auth.route('/profile')
+@login_required
 def profile():
     return render_template('auth/profile.html', name=current_user.name)
