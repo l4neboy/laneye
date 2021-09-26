bf061bc0d0c98e3b15ad5bf1778d748f439962bf
Pavel Nikishin <75571015+l4neboy@users.noreply.github.com>
2021-09-26 19:32:18 +0000

    NEW MAIN

--- main/main.py
+++ main/main.py
@@ -2,6 +2,10 @@ from flask import Blueprint, render_template, redirect, url_for
 
 main = Blueprint('main', __name__, template_folder='templates') #check
 
+@main.route('/main')
+def main():
+    return 'MAIN'
+
 @main.route('/test')
 def test():
     return 'Feel Good Inc.'
