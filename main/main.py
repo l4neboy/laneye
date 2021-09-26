de8e45ba6b2fd78cd05ced57f2714d8e11ca3b2d
Pavel Nikishin <75571015+l4neboy@users.noreply.github.com>
2021-09-26 19:20:46 +0000

    add new main route

--- main/main.py
+++ main/main.py
@@ -2,6 +2,10 @@ from flask import Blueprint, render_template, redirect, url_for
 
 main = Blueprint('main', __name__, template_folder='templates') #check
 
+@main.route('/main')
+def main():
+    return render_template('main/index.html')
+
 @main.route('/test')
 def test():
     return 'Feel Good Inc.'
