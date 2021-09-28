08a4e69ae28d946109f8b58c29fc0be0de3bd623
Pavel Nikishin <75571015+l4neboy@users.noreply.github.com>
2021-09-28 20:52:57 +0000

    update base, laneye

--- laneye.py
+++ laneye.py
@@ -25,15 +25,14 @@ def load_user(user_id):
     # since the user_id is just the primary key of our user table, use it in the query for the user
     return User.query.get(int(user_id))
 
-
-'''@app.route("/")
-def index():
-    items = Item.query.order_by(Item.price).all()
-    return render_template('index.html', data=items)'''
-
 @app.route("/")
 def index():
     return render_template('main/index.html')
+
+@app.route("/misterything")
+def index():
+    items = Item.query.order_by(Item.price).all()
+    return render_template('index.html', data=items)
 
 @app.route('/about')
 def about():
