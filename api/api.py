69c3ee0a8396aa0d51a97f9b331bb1113978ae06
Pavel Nikishin <75571015+l4neboy@users.noreply.github.com>
2021-09-29 19:41:21 +0000

    fix api

--- api/api.py
+++ api/api.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, redirect, url_for
 
-main = Blueprint('api', __name__, template_folder='templates') #check
+api = Blueprint('api', __name__) #check
 
 @api.route('/test')
 def test():
