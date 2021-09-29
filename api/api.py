def357ad85aabb6d077836dd679725eeb1ba7ba5
Pavel Nikishin <75571015+l4neboy@users.noreply.github.com>
2021-09-29 21:28:11 +0000

    upadate api

--- api/api.py
+++ api/api.py
@@ -2,6 +2,25 @@ from flask import Blueprint, render_template, redirect, url_for
 
 api = Blueprint('api', __name__) #check
 
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web', 
+        'done': False
+    }
+]
+
 @api.route('/test')
 def test():
     return 'Feel Good Inc.'
+
+@api.route('/todo/api/v1.0/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
