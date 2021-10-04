6fd9cc38691960bdd616fa40a633c0ee24400b93
Pavel Nikishin <75571015+l4neboy@users.noreply.github.com>
2021-10-04 20:19:09 +0000

    remove second api route

--- api/api.py
+++ api/api.py
@@ -25,9 +25,4 @@ def test():
 def get_tasks():
     return jsonify({'tasks': tasks})
 
-@api.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
-def get_task(task_id):
-    task = filter(lambda t: t['id'] == task_id, tasks)
-    if len(task) == 0:
-        abort(404)
-    return jsonify({'task': task[0]})
+
