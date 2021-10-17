02086f5c9cc1c4971ef4e953b1d0c4d1c15c11a4
Pavel Nikishin <n1kishin@yandex.ru>
2021-10-17 20:29:22 +0300

    upd api

--- api/api.py
+++ api/api.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, Flask, render_template, redirect, url_for, jsonify, abort
+from flask import Blueprint, Flask, request, render_template, redirect, url_for, jsonify, abort
 from flask_restful import Resource, Api
+from models import db, BookModel
 
 api_bp = Blueprint('api', __name__) #check
 api = Api(api_bp)
@@ -8,6 +9,19 @@ class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
 
-api.add_resource(HelloWorld, '/api')
+class BooksList(Resource):
+    def get(self):
+        books = BookModel.query.all()
+        return {'Books': list(x.json() for x in books)}
 
+    def post(self):
+        data = request.get_json()
+        new_book = BookModel(data['name'], data['price'], data['author'])
+        db.session.add(new_book)
+        db.session.commit()
+        return new_book.json(), 201
+
+
+api.add_resource(HelloWorld, '/api')
+api.add_resource(BooksList, '/books')
 
